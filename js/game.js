04a7992619475678307e9acb09a6ad11215b9f7c
transfer76 <shina76@ukr.net>
2019-06-01 23:11:13 +0300

    Add path to png files

--- js/game.js
+++ js/game.js
@@ -1,1 +1,8 @@
-alert("hello")
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
